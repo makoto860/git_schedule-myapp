5d4e719a2d37869bcfe326db10e4d38838476616
makoto860 <93705485+makoto860@users.noreply.github.com>
2022-05-24 20:09:58 +0900

    Create routes.rb

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'schedule9s/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :schedule9s
+end
