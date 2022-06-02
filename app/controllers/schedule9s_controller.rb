98d51b016ce181f957518d1c8f38e2c3e7f71c49
EC2 Default User <ec2-user@ip-172-31-12-203.ap-northeast-1.compute.internal>
2022-06-02 04:41:52 +0000

    gitPlの練習-initialcommit

--- app/controllers/schedule9s_controller.rb
+++ app/controllers/schedule9s_controller.rb
@@ -4,49 +4,41 @@ class Schedule9sController < ApplicationController
   end
   
   def new
-    @schedule9 = Schedule9.new
+   @schedule9 = Schedule9.new
   end
- 
+  
   def create
     @schedule9 = Schedule9.new(params.require(:schedule9).permit(:title, :first_date, :final_date, :all_day, :schedule_memo))
-    
     if @schedule9.save
-      
       flash[:notice] = "スケジュールを新規登録しました"
       redirect_to :schedule9s
     else
       render "new"
     end
   end
- 
   
-  
-  
- 
   def show
     @schedule9 = Schedule9.find(params[:id])
   end
- 
+  
   def edit
     @schedule9 = Schedule9.find(params[:id])
   end
-
+  
   def update
     @schedule9 = Schedule9.find(params[:id])
     if @schedule9.update(params.require(:schedule9).permit(:title, :first_date, :final_date, :all_day, :schedule_memo))
-      flash[:notice] = "ユーザーIDが「#{@schedule9.id}」の情報を更新しました"
+      flash[:notice] = "ユーザーIDが「#{@schedule9.id}」の情報を編集しました"
       redirect_to :schedule9s
     else
       render "edit"
     end
   end
- 
+  
   def destroy
     @schedule9 = Schedule9.find(params[:id])
     @schedule9.destroy
     flash[:notice] = "スケジュールを削除しました"
     redirect_to :schedule9s
   end
-
-  
 end
--- app/models/schedule9.rb
+++ app/models/schedule9.rb
@@ -2,13 +2,19 @@ class Schedule9 < ApplicationRecord
   validates :title, presence: true
   validates :first_date, presence: true
   validates :final_date, presence: true
-
-
-  def final_date_after_today_date
+  
+  validate :final_date_after_today
+  
+  def final_date_after_today
+    if final_date == nil
+      errors.add(:final_date,"は翌日以降の日にちを選択して") 
+    elsif final_date < Date.tomorrow
+      errors.add(:final_date,"は翌日以降の日にちを選択して") 
+    else
+      "新規登録完了処理"
+    end
     
-    errors.add(:final_date,"は翌日以降の日にちを選択して") if final_date < now = Time.current
     
   end
-  
 end
-
+      
